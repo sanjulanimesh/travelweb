ea7d220db094e068d1f4abfab72e780cb8ff3154
Sanjula Nimesh <sanjulanh@gmail.com>
2024-03-29 00:15:04 +0530

    Create quection.js

---
+++ quection.js
@@ -0,0 +1,102 @@
+//Created an array to pass number , quection , answer , options
+let quections=[{
+
+    number:1,
+    quection:"Where is Sri Lanka's most popular beach located?",
+    answer:"Negombo",
+    options:[
+        "Negombo","Sigiriya","Nuwaraeliya","Horana"
+
+    ]
+},
+{
+
+    number:2,
+    quection:"What does the renowned temple in Kandy go by?",
+    answer:"Tooth Relic Temple",
+    options:[
+        "Golden Temple","Tooth Relic Temple","Lotus Temple","Ruwanveliseya Temple"
+    ]
+},
+{
+
+    number:3,
+    quection:"What is the name of famous waterfall in Sri Lanka?",
+    answer:"Babarakanda Falls",
+    options:[
+        "nail Falls","victoria Falls","Babarakanda Falls","Diyathalava Falls"
+
+    ]
+},
+{
+
+    number:4,
+    quection:"How does art contribute to the understanding of Sri Lankan history and culture?",
+    answer:"Documentation",
+    options:[
+        "Magazine","Paper","Documentation","Book"
+
+    ]
+},
+{
+
+    number:5,
+    quection:"What are the key features of Sri Lankan cuisine?",
+    answer:"Spices",
+    options:[
+        "Food","Drugs","Medicine","Spices"
+
+    ]
+},
+{
+
+    number:6,
+    quection:"What is the Srilanka most popular food",
+    answer:"Riceandcurry",
+    options:[
+        "Kottu","Riceandcurry","Pizza","Burger"
+
+    ]
+},
+{
+
+    number:7,
+    quection:"When does the southwest monsoon (Yala) occur in Sri Lanka?",
+    answer:"May to September",
+    options:[
+        "May to September","May to january","March to May","May to December"
+
+    ]
+},
+{
+
+    number:8,
+    quection:"What are some popular tourist destinations in Sri Lanka's central highlands?",
+    answer:"Kandy",
+    options:[
+        "Horana","Kandy","Colombo","Delkada"
+
+    ]
+},
+{
+
+    number:9,
+    quection:"What are some popular tourist attraction in",
+    answer:"Historical land marks",
+    options:[
+        "Historical land marks","sea","Food","Water Falls"
+
+    ]
+},
+{
+
+    number:10,
+    quection:"Who found sigiriya?",
+    answer:"King Kashyapa",
+    options:[
+        "King ravana","King gajaba","King Kashyapa","King pandukabaya"
+
+    ]
+},
+
+];
